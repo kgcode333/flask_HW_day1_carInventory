a63925eed57d3d32b1266106ab931cd71a1ed699
kgcode333 <kimegriffin333@gmail.com>
2022-02-10 09:56:36 -0600

    forms and dbs

--- lux_inventory/__init__.py
+++ lux_inventory/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
 from .api.routes import api
 from .site.routes import site
 from .authentication.routes import auth
 from config import Config
+from .models import db as root_db
 
 app = Flask(__name__)
 
@@ -11,3 +16,7 @@ app.register_blueprint(site)
 app.register_blueprint(auth)
 
 app.config.from_object(Config)
+
+
+root_db.init_app(app)
+migrate = Migrate(app,root_db) 
